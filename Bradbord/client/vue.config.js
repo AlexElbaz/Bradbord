eaddeb44708f052f2e2c0d123dd000ea101a1872
Alex Elbaz <aelbaz@bayviewglen.ca>
2021-12-14 09:09:07 -0500

    Setup Commit - Brought in Heroku project layout

---
+++ Bradbord/client/vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    outputDir: path.resolve(__dirname, '../server/public'), // __dirname is the name of the directory a command is being called from (when we run 'npm run build' it runs from client folder, so __dirname = client).
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5000'
+            }
+        }
+    }
+}
---
+++ Bradbord/server/routes/api/posts.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get Posts
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
+});
+
+// Add post
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    
+    res.status(201).send();
+});
+
+// Delete posts
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    
+    await posts.deleteOne({ _id: new mongodb.ObjectId(req.params.id)});
+
+    res.status(200).send();
+});
+
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect('mongodb+srv://root:root@fullstack-vue-express-b.bhfde.mongodb.net/FULLSTACK-VUE-EXPRESS-BACKEND?retryWrites=true&w=majority', {
+        useNewUrlParser: true
+    });
+
+    return client.db('FULLSTACK-VUE-EXPRESS-BACKEND').collection('posts');
+}
+
+// Allows us to use router elsewhere (by imorting elsewhere)
+module.exports = router;
