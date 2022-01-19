0befbb720b1c41c231c27da2e539ebce323dda7b
Aaron Chow <achow@bayviewglen.ca>
2022-01-18 23:15:39 -0500

    Styled some Posts

--- Toucan/client/src/components/Post.vue
+++ Toucan/client/src/components/Post.vue
@@ -1,14 +1,25 @@
 <template>
-    <div class="card mx-auto p-3 my-2">
-        <div class="card-title h2 post-title d-flex justify-content-between align-items-center px-3">
-            {{post.title}} - {{post.dueDate}} at {{post.dueTime}}
-            <button class="btn btn-danger" @click="$emit('delete-post', post._id)">Delete Post</button>
+<div>
+    <div class="card p- my-2">
+        <div class="card-body">
+            <div class="d-flex">
+            <h3 class="card-title mx-auto fw-bolder">{{post.title}}</h3>
+            <button class="btn btn-close text-right" 
+            @click="$emit('delete-post', post._id)"></button>
+            </div>
+            <p class="">{{post.dueDate}} at {{post.dueTime}}</p>
+            <div class=" text-start">
+            <hr>
+            {{ post.body }} 
+            <hr>
+            <p class="fst-italic"> date: {{ post.createdAt.toString().substring(0, 16) }} </p>
+            </div>
         </div>
-        <hr>
-        <div class="card-body text-start">
-            {{ post.body }} - created at: {{ post.createdAt.toString().substring(0, 24) }}
+       
         </div>
     </div>
+    <!-- Modal -->
+    
 </template>
 
 <script>
@@ -21,5 +32,30 @@ export default {
 </script>
 
 <style scoped>
+
+hr{
+    border-top: 1px dashed grey;
+}
+
+.card{
+    border-radius: 20px;
+    background: #f3ffed;
+    border: 1px solid rgba(100, 100, 100, 0.25)
+}
+
+i {
+  font-size: 25px;
+}
+
+.logo{
+  position: absolute;
+  width: 45%;
+  left: 25%;
+  top: 100px;
+}
+
+.modal-dialog{
+  position: relative;
+}
 
 </style>
