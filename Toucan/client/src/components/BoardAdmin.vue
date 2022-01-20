18ede1b8abf303ffc213fa27d7288966ff04143c
Trevor Cheung <tcheung@bayviewglen.ca>
2022-01-20 16:55:20 -0500

    Fixed navbar and made it slightly cleaner

--- Toucan/client/src/components/BoardAdmin.vue
+++ Toucan/client/src/components/BoardAdmin.vue
@@ -1,25 +1,147 @@
 <template>
+<div id="bg">
   <div class="container">
-    <header class="jumbotron">
+    <NavbarComponent />
+    <div v-if="isAdmin" class="row">
+      <div class="col-lg-4">
+        <AddCourse @add-course="addCourse"/>
+        <Courses
+        @delete-course="deleteCourse"
+        @show-posts="showPosts"
+        @show-all-posts="showAllPosts"
+        :courses="courses"
+        />
+      </div>
+      <div class="col-lg-8">
+        <div id="post-header"></div>
+        <CourseTabs :posts="filteredPosts" @delete-post="deletePost" @re-render-posts="forceRerender" :course="selectedCourse" :isSelected="isSelected"/>
+      </div>  
+    </div>
+    <div class="row" v-if="!isAdmin">
       <h3>{{ content }}</h3>
-    </header>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
-import UserService from "../services/user.service";
+import AddCourse from '@/components/AddCourse.vue'
+import Courses from '@/components/Courses.vue'
+import NavbarComponent from '@/components/NavbarComponent.vue'
+import CourseTabs from '@/components/CourseTabs.vue'
+
+import PostService from '@/PostService.js'
+import CourseService from '@/CourseService.js'
+
+import UserService from "../services/user.service"
 
 export default {
-  name: "User",
+  name: 'Home',
+  components: {
+    AddCourse,
+    Courses,
+    NavbarComponent,
+    CourseTabs
+  },
   data() {
     return {
+      courses: [],
+      posts: [],
+      filteredPosts: [],
+      selectedCourse: '',
+      isSelected: false,
       content: "",
-    };
+      isAdmin: false,
+    }
+  },
+  methods: {
+    async addCourse(course) {
+      await CourseService.insertCourse(course.name, course.courseCode, course.teacher, course.members, course.time, course.img, course.modalID);
+      this.courses = await CourseService.getCourses();
+    },
+    async deleteCourse(id) {
+      await CourseService.deleteCourse(id); // Deletes the course
+      this.courses = await CourseService.getCourses(); // Gets all the courses again now that a specific course has been deleted
+
+      // Find all the posts that belong to the deleted course and deletes them one by one
+      this.posts.forEach(async post => {
+        if (post.courseID === id)
+          await PostService.deletePost(post._id);
+      });
+      
+      this.posts = (await PostService.getPosts()).reverse();  // Gets all the posts again now that all the posts belonging to the deleted course have been deleted
+      this.filteredPosts = this.posts; // Sets filteredPosts (determines which posts are displayed) to all remaining posts
+    },
+    async showPosts(id) {
+      // old version of showPosts without using component; may revert back to this if posts component fails
+      /*document.querySelector('#post-header').innerHTML = '';
+      document.querySelector('#posts').innerHTML = '';
+      //let currentCourse = await CourseService.getCourse(id);
+      this.posts = await PostService.getPosts();
+      console.log(this.posts);
+      console.log(this.courses);
+
+      const courseName = this.getCourseName(id);
+      this.createPostsHeader(courseName);
+      
+      this.posts.forEach((post) => {
+        console.log(post.courseID);
+        if (post.courseID === id) {
+          let coursePost = this.showPost(post, id);
+          console.log(coursePost);
+          document.querySelector('#posts').appendChild(coursePost);
+        }
+      })*/
+      this.selectedCourse = id;
+      this.isSelected = true;
+      this.filteredPosts = this.posts.filter(post => (post.courseID === id));
+    },
+    showAllPosts() {
+      this.isSelected = false;
+      this.filteredPosts = this.posts;
+    },
+    getCourseName(id) {
+      let courseName = '';
+      this.courses.forEach((course) => {
+        if (course._id === id) {
+          courseName = course.name;
+        }
+      })
+      return courseName;
+    },
+    async deletePost(id) {
+      console.log(id);
+
+      let courseOfPost;
+      this.posts.forEach(post => {
+        if (post._id === id)
+          courseOfPost = post.courseID;
+      });
+
+      await PostService.deletePost(id);
+
+      this.posts = (await PostService.getPosts()).reverse();
+
+      if (this.isSelected) // Makes it so that if a course is selected, displays those posts, otherwise dispaly all posts
+        this.filteredPosts = this.posts.filter(post => (post.courseID === courseOfPost));
+      else
+        this.filteredPosts = this.posts;
+    },
+    async forceRerender(id) {
+      this.posts = (await PostService.getPosts()).reverse();
+      this.showPosts(id)
+    }
+  },
+  async created() {
+    this.courses = await CourseService.getCourses();
+    this.posts = (await PostService.getPosts()).reverse();
+    this.filteredPosts = this.posts;
   },
   mounted() {
     UserService.getAdminBoard().then(
       (response) => {
-        this.content = response.data;
+        this.isAdmin = true;
+        console.log('test');
       },
       (error) => {
         this.content =
@@ -31,5 +153,29 @@ export default {
       }
     );
   },
-};
+}
 </script>
+
+<style scoped>
+div.container {
+  max-width: 80%;
+  margin: 0 auto;
+}
+
+div.post-title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
+
+<style>
+
+body{
+    background: url(../assets/homebg.png) no-repeat;
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+    height: 100vh;
+  }
+</style>
